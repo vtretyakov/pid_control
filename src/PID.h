0180b772d027425c3fbd12598dc6b0ee1041f576
vtretyakov <vtretyakov@synapticon.com>
2018-02-16 12:08:40 +0100

    first functional implementation
    not too smooth but already works, further tuning is required

--- src/PID.h
+++ src/PID.h
@@ -6,16 +6,18 @@ public:
   /*
   * Errors
   */
-  double p_error;
-  double i_error;
-  double d_error;
+  double p_error_;
+  double i_error_;
+  double d_error_;
+  double last_error_;
+  double integral_limit_;
 
   /*
   * Coefficients
   */ 
-  double Kp;
-  double Ki;
-  double Kd;
+  double Kp_;
+  double Ki_;
+  double Kd_;
 
   /*
   * Constructor
@@ -30,12 +32,17 @@ public:
   /*
   * Initialize PID.
   */
-  void Init(double Kp, double Ki, double Kd);
+  void Init(double Kp, double Ki, double Kd, double integral_limit);
 
   /*
   * Update the PID error variables given cross track error.
   */
   void UpdateError(double cte);
+  
+  /*
+   * Update the PID error variables given reference and current values.
+   */
+  void UpdateError(double ref_value, double current_value);
 
   /*
   * Calculate the total PID error.
